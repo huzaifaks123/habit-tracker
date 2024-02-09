bd8ca54cd016fc4854c41295a701abe71477ed1d
huzaifaks123 <huzaifaks123@gmail.com>
2024-02-10 04:27:29 +0530

    add

--- src/Components/DetailList.jsx
+++ src/Components/DetailList.jsx
@@ -52,8 +52,8 @@ export default function DetailList() {
                     <div className={styles.detailContainer} key={index}>
                         <input type="checkbox" checked={check === index ? true : false} onChange={() => handleChecked(index)} className={styles.checkBox}></input>
                         <div className={styles.leftContainer}>
-                            <div onClick={() => handleDelete(habit)} className={check === index ? styles.visDeleteContainer : styles.hidDeleteContainer}>
-                                <img className={check === index ? styles.actDelImg : styles.InActDelImg} src="https://cdn-icons-png.flaticon.com/128/7267/7267205.png" alt="delImg"></img>
+                            <div className={check === index ? styles.visDeleteContainer : styles.hidDeleteContainer}>
+                                <img onClick={() => handleDelete(habit)} className={check === index ? styles.actDelImg : styles.InActDelImg} src="https://cdn-icons-png.flaticon.com/128/7267/7267205.png" alt="delImg"></img>
                             </div>
                             <div className={styles.title}>{habit.title.text} {habit.title.img ? <img src={habit.title.img} className={styles.imgTag} alt="catImg" /> : ""}</div>
                             <div className={styles.rightContainer}>
